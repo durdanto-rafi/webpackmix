699b3529a7ff1304eb4330a2bde5766681917e2f
Golam Mahmud Rafi <rafi.aust@live.com>
2018-04-16 17:40:11 +0900

    theme added

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <Dashboard></Dashboard>
+</template>
+
+<script>
+    import Dashboard from './Dashboard'
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        components: {
+            Dashboard
+        }
+    }
+</script>
